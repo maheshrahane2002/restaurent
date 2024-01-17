f52e5e8b980bc9c20facf87439788b9b6ef643f3
technOrbit <maheshrahane65@gmail.com>
2024-01-17 19:45:25 +0530

    Initial commit

---
+++ resources/views/admin/foodmenu.blade.php
@@ -0,0 +1,54 @@
+<x-app-layout>
+
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+
+  @include("admin.admincss")
+
+   
+  </head>
+  <body>
+
+  <div class="container-scroller">
+
+    @include("admin.navbar")
+    
+    <div style="position: relative; top: 60px; right: -150px">
+        <form action="{{url('/upload food')}}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <label>Title:</label>
+                <input style="color:blue;" type="text" name"title" placeholder="Write a title" required>
+            </div>
+
+            <div>
+                <label>Price:</label>
+                <input style="color:blue;"type="num" name"price" placeholder="price" required>
+            </div>
+
+            <div>
+                <label>Image:</label>
+                <input type="file" name"image"  required>
+            </div>
+
+            <div>
+                <label>Description:</label>
+                <input style="color:blue;"type="text" name"description" placeholder="description" required>
+            </div>
+
+            <div>
+                <input style="color:red;"type="submit" value="Save">
+            </div>
+        </form>
+    </div>
+</div>
+
+
+    @include("admin.adminscript")
+
+    
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get("/",[HomeController::class,"index"]);
+
+Route::get("/users",[AdminController::class,"user"]);
+
+Route::get("/foodmenu",[AdminController::class,"foodmenu"]);
+
+Route::post("/uploadfood",[AdminController::class,"upload"]);
+
+Route::get("/deleteuser/{id}",[AdminController::class,"deleteuser"]);
+
+Route::get("/redirects",[HomeController::class,"redirects"]);
+
+
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
